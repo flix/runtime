9dd764d13fe4fcd98e5c4ebcae97e620a2a5f9e6
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2018-06-05 10:00:08 +0200

    Added Match and Switch exceptions.

---
+++ src/main/java/flix/runtime/MatchException.java
@@ -0,0 +1,9 @@
+package flix.runtime;
+
+public final class MatchException extends FlixException {
+
+    public MatchException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/flix/runtime/SwitchException.java
@@ -0,0 +1,9 @@
+package flix.runtime;
+
+public final class SwitchException extends FlixException {
+
+    public SwitchException(String message) {
+        super(message);
+    }
+
+}
